8af94e9cb42199dd86eaa6bf6392f08b1754109c
Kade Hurst <kadehurst@Kades-MacBook-Pro.local>
2021-04-19 17:13:57 -0600

    k

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
  
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
   
         // Set up the Router
