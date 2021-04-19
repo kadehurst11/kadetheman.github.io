40b75d82bf26c63f7de50b5c2bce8d3941265855
Kade Hurst <kadehurst@Kades-MacBook-Pro.local>
2021-04-19 15:19:53 -0600

    k

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React from 'react';
 import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Home from './home.js'; 
+import Kevin from './kevin.js'; 
+import Nuggets from './nuggets.js'; 
 import './App.css';
-
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+  
+        // Set up the Router
+        <Route exact path="/" component={Home} />
+        <Route path="/articles" component={Kevin} />
+        <Route path="/about" component={Nuggets} />
+  
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+                                          
+            // Set up the Links
+            <Link to="/" className="item">Home</Link>
+            <Link to="/articles" className="item">Kevin</Link>
+            <Link to="/about" className="item">Nuggets</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
